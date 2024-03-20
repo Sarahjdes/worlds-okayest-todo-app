bcca4843709cc7d7cde2c55559fa08385b89b6a2
Sarah-Jeanne <>
2024-03-20 11:01:14 -0400

    added new page with a from to add an entry

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,44 @@
 class TodosController < ApplicationController
   # Action for the index view
   def index
+    # Selects all items
     @todos = Todo.all
   end
 
   def show
+    # Selects an item depending on the :id param of the /todos/show/:id url
     @todo = Todo.find(params[:id])
   end
+
+  def new
+    # Instentiates a new entity
+    @todo = Todo.new
+  end
+
+  # Action on submit
+  def create
+    # Instentiates a new entity with the params provided in the form
+    @todo = Todo.new(todo_params)
+
+    # Tries to save instance previously created
+    if @todo.save
+      # Redirects back to show page for that record
+      # If redirect_to is passed a record, the url will be generated using a helper
+      # redirect_to creates a new request, which we want to do after mutating the db or application state
+      # so the request is not done twice if the page is refreshed by the user
+      redirect_to @todo
+    else
+      # Will render new view with an unprocessable_entity status
+      render :new, status: unprocessable_entity
+    end
+  end
+
+  private
+    def todo_params
+      # params[:todo][:content]
+      params.require(:todo).permit(:content)
+
+      # Strong typing allows to pass an array or params in permit
+      # params.require(:todo).permit(:content, :another_param)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,15 @@ Rails.application.routes.draw do
 
   # Creates routes for the standard CRUD actions as well as _path and _url helpers
   resources :todos
+    # index     todos       /todos
+    # show      todo        /todos/:id
+    # new       new_todo    /todos/
+    # edit      edit_todo   /todos/:id/edit
+    # create    on submit
+    # update    on submit
+    # destroy
+
+    # helpers
+      # - todos_path    used to generate path in the application (in views)
+      # - todos_url     used to generate urls like for redirects or external links (in controller)
 end
