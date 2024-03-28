5ad791b054c5727a323da58d64c96579469a45f1
Sarah-Jeanne <>
2024-03-28 11:40:23 -0400

    reducing size of code

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -6,34 +6,26 @@ RSpec.describe Todo, type: :model do
   # This creates test data using the factory
   let(:todo) { build(:todo) }
 
+  # Tests if the instance created by the factory is valid
   it 'has a valid factory' do
     expect(todo).to be_valid
   end
 
-  describe '#title' do
-    subject { todo.title }
+  # This tests the validity of the data created through the factory
+  it { is_expected.to have_attribute(:title) }
+  it { is_expected.to have_attribute(:content) }
 
-    # This tests the validity of the data created through the factory
-    it { is_expected.to eq 'Some title' }
-  end
+  describe 'validation' do
+    it 'does not allow empty title' do
+      # From the data we have in the factory, we're only changing the value we want to test
+      # Allows to not repeat the valid data AND reduce the chance of making mistakes
+      expect(build(:todo, title: nil)).to_not be_valid
+      expect(build(:todo, title: '')).to_not be_valid
+    end
 
-  describe '#content' do
-    subject { todo.content }
-
-    # This tests the validity of the data created through the factory
-    it { is_expected.to eq 'This is a thing to do' }
-  end
-
-  # This tests other use cases
-  it 'has valid title' do
-    expect(Todo.new(title: nil, content: 'And this is some explanation')).to_not be_valid
-    expect(Todo.new(title: '', content: 'And this is some explanation')).to_not be_valid
-    expect(Todo.new(title: 'This needs to be done', content: 'And this is some explanation')).to be_valid
-  end
-
-  it 'has valid content' do
-    expect(Todo.new(title: 'This needs to be done', content: nil)).to_not be_valid
-    expect(Todo.new(title: 'This needs to be done', content: '')).to_not be_valid
-    expect(Todo.new(title: 'This needs to be done', content: 'And this is some explanation')).to be_valid
+    it 'does not allow empty content' do
+      expect(build(:todo, content: nil)).to_not be_valid
+      expect(build(:todo, content: '')).to_not be_valid
+    end
   end
 end
