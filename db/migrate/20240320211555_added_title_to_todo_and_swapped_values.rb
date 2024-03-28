8b053b5be8a83e20cfcd68fb8c16cab2345ea6c2
Sarah-Jeanne <>
2024-03-28 11:22:43 -0400

    when using change_column in a migration, an up/down is needed so migration can be rolledback

--- db/migrate/20240320211555_added_title_to_todo_and_swapped_values.rb
+++ db/migrate/20240320211555_added_title_to_todo_and_swapped_values.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 
 class AddedTitleToTodoAndSwappedValues < ActiveRecord::Migration[7.1]
-  def change
+  def up
     change_table :todos do |t|
       t.rename :content, :title
       t.text :content
     end
 
-    # Next time, either use up/down methogs or reversible method
     change_column :todos, :title, :string
+  end
+
+  def down
+    change_column :todos, :title, :text
+
+    change_table :todos do |t|
+      t.rename :title, :content
+      t.remove :title
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_320_211_555) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_20_211555) do
   create_table 'todos', force: :cascade do |t|
     t.string 'title'
     t.datetime 'created_at', null: false
